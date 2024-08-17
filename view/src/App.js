5da6354cb4c67c703c5ffd14605e8c627cb28614
minju0236 <minju@gmail.com>
2024-08-17 19:22:34 +0900

    📝Add or update documentation

--- view/src/App.js
+++ view/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './login/login.js';
+import ForestDiary from './diaries/diaries_forest.js';
+import CityDiary from './diaries/diaries_city.js';
+import SeaDiary from './diaries/diaries_sea.js';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        {/* 로그인 페이지 경로 */}
+        <Route path="/" element={<Login />}></Route>
+        {/* 숲 다이어리 페이지 경로 */}
+        <Route path="/forest_diary" element={<ForestDiary />}></Route>
+        {/* 도시 다이어리 페이지 경로 */}
+        <Route path="/city_diary" element={<CityDiary />}></Route>
+        {/* 바다 다이어리 페이지 경로 */}
+        <Route path="/sea_diary" element={<SeaDiary />}></Route>        
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
